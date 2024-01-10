e640f984420c2fb052bcb0336d488fa32ef51093
Aliyu Abdulkabir <abdulkabiraliyu@gmail.com>
2024-01-10 15:17:12 -0800

    Advanced task

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
 def multiply_list_map(my_list=[], number=0):
-    """all values multiplied by a number without using any loops"""
-
     return list(map(lambda n: n * number, my_list))
